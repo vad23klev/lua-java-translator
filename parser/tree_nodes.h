7f9d213a38f1cdee684666ab440506dcc455ec51
vad23klev <vad23klev@gmail.com>
2014-03-22 14:08:01 +0000

    обновлено перечистление типов выражения.

--- parser/tree_nodes.h
+++ parser/tree_nodes.h
@@ -45,7 +45,7 @@ struct NFunc
 
 enum NStmtType {OP,EXPR,FUNC};
 
-enum NExprType {EQ,NQ,PLUS,MINUS,DIV,MUL,LT,GT,MOD,ID,NUM,CONC,STR,MET,AND,NOT,OR,MAS};
+enum NExprType {EQ,NQ,PLUS,MINUS,DIV,MUL,LTE,GTE,LT,GT,MOD,ID,NUM,CONC,STR,MET,AND,NOT,OR,MAS};
 
 struct NExpr* create_op_expr(NExprType type,NExpr* left,NExpr* right)
 {
