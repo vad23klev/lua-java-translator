f69f2c77e55be2bddacc1a1d5e7c1098ba90efd0
vad23klev <vad23klev@gmail.com>
2014-03-22 20:03:39 +0000

    В файле с узлами дерева добавлены элементы перечисления типов выражения.

--- parser/tree_nodes.h
+++ parser/tree_nodes.h
@@ -45,7 +45,7 @@ struct NFunc
 
 enum NStmtType {OP,EXPR,FUNC};
 
-enum NExprType {EQ,NQ,PLUS,MINUS,DIV,MUL,LE,GE,LT,GT,MOD,ID,NUM,CONC,STR,MET,AND,NOT,OR,MAS};
+enum NExprType {EQ,NQ,PLUS,MINUS,DIV,MUL,LE,GE,LT,GT,MOD,ID,INT,DOUBLE,CONC,STR,MET,AND,NOT,OR,MAS};
 
 struct NExpr* create_op_expr(NExprType type,NExpr* left,NExpr* right)
 {
