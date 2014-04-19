136abdc9032795ba21d19366394ecee0c29f8ead
vad23klev <vad23klev@gmail.com>
2014-04-19 13:06:03 +0100

    добавлены тесты

---
+++ parser/tests/test1.lua
@@ -0,0 +1,14 @@
+function shellsort(t, befo1re, n1)
+    n = n
+
+    k,z,c,v = 0x3F,'sada\n',"sdda\\\"\n", [[
+    sdasd asd asd asd asd
+    dasdasd \n\"]]
+    if not before or before == "<" then return ssup(t, n)
+    elseif before == ">" then return ssdown(t, n)
+    else return ssgeneral(t, n, before)
+    end
+    ins.a.d[r.t]
+    ina[a]
+    return shellsort
+end
---
+++ parser/tests/test2.lua
@@ -0,0 +1,15 @@
+function fib(n)
+
+
+
+
+if n < 2 then return 1
+    elseif n > 0 then
+    n = 3
+
+
+
+    else n = 2
+    end
+    return fib ( n - 2 ) + fib ( n - 1 )
+end
