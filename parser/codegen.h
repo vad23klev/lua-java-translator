17702343ec2b4157272dd926b1b73fd95283b4c6
nixorv <nixorv@gmail.com>
2014-06-22 14:48:34 +0400

    Добавлены некоторые сущности в codegen

--- parser/codegen.h
+++ parser/codegen.h
@@ -6,4 +6,29 @@
 #ifndef _H_CODEGEN_
 #define _H_CODEGEN_
 
+/**
+ * Provides numbers of `methodref` constants on RTL functions and methods.
+ */
+enum cg_rtl_methodref {
+    // Mixed type methods
+    MIXED_FUNC = 1,
+
+    // Library functions
+    LIB_PRINT = 10000
+};
+
+/**
+ * Generates .class-files for given tree (with function .class-files).
+ * @pre The tree must be prepared with st_fill_tables() function.
+ * @param [in] root Tree root.
+ */
+void cg_generate_bytecode(struct NStmtList * root);
+
+/***************************************************************************************************/
+
+void cg_generate_bytecode(struct NStmtList * root) {
+    // Not implemented yet.
+}
+
 #endif
+
