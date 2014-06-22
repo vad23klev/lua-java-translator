8974d0081e452851251f31c1bccc065c63abc7d7
vad23klev <vad23klev@gmail.com>
2014-06-22 14:34:42 +0100

    добавлен файл с кодом для генерации программы

---
+++ parser/codegen.h
@@ -0,0 +1,10 @@
+/**
+ * @file semantic_tables.h
+ * Provides functions to create various tables (e. g. constants,
+ * local variables) for .class files.
+ */
+
+#ifndef _H_CODEGEN_
+#define _H_CODEGEN_
+
+#endif
